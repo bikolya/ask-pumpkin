178e9afda223065a1b2e8e2400aa0d754e79395a
Nikolay Bienko <bikolya@gmail.com>
2014-12-20 18:53:40 +0300

    add simple auth logic to User

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+  before_filter :authenticate_user!
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   root to: 'home#index'
+  resources :users, except: [:index]
 end
