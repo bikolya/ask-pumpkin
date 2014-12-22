4863772467c1bf35311afadf736a8aa578b7af43
Nikolay Bienko <bikolya@gmail.com>
2014-12-23 00:35:27 +0300

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
 
   has_many :questions
   has_many :answers
+
+  validates :email, :password, :username, :nickname, presence: true
+  validates :password, length: { minimum: 3, maximum: 32 }
+  validates :username, :nickname, length: { maximum: 16 }
 end
