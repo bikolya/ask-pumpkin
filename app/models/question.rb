e9511898b95696372cfe2c9342733fdb9ff9bbba
Nikolay Bienko <bikolya@gmail.com>
2014-12-27 17:13:38 +0300

    update models and db:seed

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 class Question < ActiveRecord::Base
   belongs_to :user
   has_many :answers, dependent: :destroy
+  has_many :likes, as: :likable
+  has_many :tags, as: :taggable
 
   validates :title, :body, presence: true
   validates :title, length: { maximum: 200 }
